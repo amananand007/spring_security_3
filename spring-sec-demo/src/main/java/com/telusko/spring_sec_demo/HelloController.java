e5f55bb1701b45c61dd0f7857bdc026398e5e11f
Aman Anand <amananand2751@gmail.com>
2025-06-11 08:43:06 +0530

    spring security initial commit csrf token

---
+++ spring-sec-demo/src/main/java/com/telusko/spring_sec_demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.telusko.spring_sec_demo;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String greet(HttpServletRequest request){
+        return "Hello world " + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Telusko " + request.getSession().getId();
+    }
+}
